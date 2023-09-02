295b6d01d7140532342b63340abad49beb914f19
user-name <user-email>
2023-09-02 15:48:22 -0400

    Add react-social-media-embed

--- src/screen/Home.tsx
+++ src/screen/Home.tsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { AppBar, Toolbar, Typography, Container, Switch } from '@mui/material';
 import { AccountCircle, Route, Router } from '@mui/icons-material';
 import { getTodaysFame } from '../api/ApiClient';
+import FrameCard from '../components/MainView/FrameCard';
+import YoutubeCard from '../components/YoutubeCard';
+import TwitterCard from '../components/TwitterCard';
+import { InstagramEmbed, TwitterEmbed, YouTubeEmbed } from 'react-social-media-embed';
 import ImageCard from '../components/MainView/ImageCard';
 
 export type ImageFields = {
@@ -21,13 +25,6 @@ function Home() {
 
   return (
       <div className="App">
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6">Material-UI React App</Typography>
-            <div style={{ flexGrow: 1 }} />
-            <AccountCircle />
-          </Toolbar>
-        </AppBar>
         <Container maxWidth="sm" style={{ marginTop: '2rem' }}>
           <Typography variant="h4" component="h1" gutterBottom>
             Welcome to Material-UI!
@@ -35,10 +32,23 @@ function Home() {
           <Typography variant="body1">
             This is a simple example of using Material-UI components in a React app.
           </Typography>
-          <ImageCard 
-            id={"sd"} 
-            link="" 
-            image={data?.image||""} />
+
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <ImageCard link='' id='' image='https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fHww&w=1000&q=80' />
+          </div>
+             
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <YouTubeEmbed url="https://www.youtube.com/watch?v=HpVOs5imUN0" width={560} height={315} />
+          </div>
+          
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <TwitterEmbed url="https://twitter.com/MichelleObama/status/1179770850754596865?s=20" width={"100%"} />
+          </div>
+
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <InstagramEmbed url="https://www.instagram.com/p/CUbHfhpswxt/" width={"100%"} />
+          </div>
+
         </Container>
       </div>
   );
