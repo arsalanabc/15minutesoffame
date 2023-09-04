439c692bae13f90c569d8342d3b1f864b1e8a77f
user-name <user-email>
2023-09-04 14:10:12 -0400

    Add 404 page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,12 +3,21 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './screen/Home'
 import Submit from './screen/submit'
 
+function PageNotFound (): React.ReactElement {
+  return <div>
+    <h1>404 Error</h1>
+    <h1>Page Not Found</h1>
+  </div>
+}
+
 function AppRoutes (): React.ReactElement {
   return (
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/submit/:id" element={<Submit />} />
           <Route path="/submit" element={<Submit />} />
+          <Route path="/*" element={<PageNotFound />} />
+
         </Routes>
   )
 }
